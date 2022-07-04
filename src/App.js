8eaf182b233d838906e1d3e25a2b38fc707e180f
Juan Carlos <juancarlos.gonzalezhuerta@aol.com>
2022-07-04 14:10:43 -0500

    feature: services and app modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Navbar } from "./navbar";
+import { Bienvenido } from "./pestañas/Bienvenido";
+import { Estudiante } from "./pestañas/Estudiante";
+import { Pokemons } from "./pestañas/Pokemons";
+import { Pokemon } from "./pestañas/Pokemon";
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [id, setId] = useState(800)
+  const [pokemon, setPokemon] = useState({
+
+  });
+
+  useEffect(() => {
+    const get = async () => {
+      const data = await axios.get("https://pokeapi.co/api/v2/pokemon/800")
+      setPokemon({
+        name: data.data.name,
+        image: data.data.sprites.front_default,
+        experience: data.data.base_experience,
+        weight:data.data.weight,
+      })
+      return data.data
+    }
+    get()
+    }, []);
+
+
+  const obtenerInformacion = async (id) => {
+    console.log(id)
+      const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      setPokemon({
+        name: data.data.name,
+        image: data.data.sprites.front_default,
+        experience: data.data.base_experience,
+        weight:data.data.weight,
+      });      console.log(data.data)
+    let a = id + 1;
+    setId(a)
+  }
+
+  const pokemonReducir = async (id) => {
+    if(id<0){
+      return
+    }
+    else{
+      const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      setPokemon({
+        name: data.data.name,
+        image: data.data.sprites.front_default,
+        experience: data.data.base_experience,
+        weight:data.data.weight,
+
+      });      console.log(data.data)
+      console.log(data.data.abilities)
+    let a = id - 1;
+    setId(a)
+    }
+  }
+  
+    return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Bienvenido />} />
+          <Route path="/pokemons" element={<Pokemons />} />
+          <Route path="/pokemon/:id" element={<Pokemon />} />
+          <Route path="/191542" element={<Estudiante />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
